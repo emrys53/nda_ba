ec73b0da2801eb671bcf38f5e01ddc25ebf4657f
Fikret Ardal <fikret.ardal@tum.de>
2025-04-16 06:26:48 +0300

    fix a bug in mock_simd class

--- c++/nda/simd/mock_simd.hpp
+++ c++/nda/simd/mock_simd.hpp
@@ -11,31 +11,31 @@ namespace nda::simd {
     template <typename... Args>
     value_t operator()(Args &&...args) const {
       static_assert((std::is_same_v<value_t, std::remove_cvref_t<Args>> and ...), "All types have to be the same.");
-      return static_cast<Derived *>(this)->operator()(std::forward<Args>(args)...);
+      return static_cast<const Derived *>(this)->operator()(std::forward<Args>(args)...);
     }
 
     private:
-    [[gnu::always_inline]] std::array<value_t, simd_t::size()> convert_simd_to_array(const simd_t &a) {
+    [[gnu::always_inline]] std::array<value_t, simd_t::size()> convert_simd_to_array(const simd_t &a) const  {
       alignas(simd_t::alignment()) std::array<T, simd_t::size()> result;
       a.store(result.data());
       return result;
     }
 
     template <size_t... Is, typename... Args>
-    [[gnu::always_inline]] auto make_array_tuple(std::index_sequence<Is...>, const std::tuple<Args...> &args_tuple) {
+    [[gnu::always_inline]] auto make_array_tuple(std::index_sequence<Is...>, const std::tuple<Args...> &args_tuple) const {
       return std::make_tuple(convert_simd_to_array(std::get<Is>(args_tuple))...);
     }
 
     template <size_t... Is, typename... Args>
-    [[gnu::always_inline]] auto apply_function(std::index_sequence<Is...>, const std::tuple<Args...> &array_tuple) {
+    [[gnu::always_inline]] auto apply_function(std::index_sequence<Is...>, const std::tuple<Args...> &array_tuple) const {
       alignas(simd_t::alignment()) std::array<T, simd_t::size()> result_array;
-      for (int i = 0; i < simd_t::size(); ++i) { result_array[i] = static_cast<Derived *>(this)->operator()(std::get<Is>(array_tuple)[i]...); }
+      for (int i = 0; i < simd_t::size(); ++i) { result_array[i] = static_cast<const Derived *>(this)->operator()(std::get<Is>(array_tuple)[i]...); }
       return result_array;
     }
 
     public:
     template <typename... Args>
-    simd_t load(Args &&...args) {
+    simd_t load(Args &&...args) const {
       static_assert((std::is_same_v<simd_t, std::remove_cvref_t<Args>> and ...), "All types have to be the same.");
       constexpr size_t args_size        = sizeof...(Args);
       std::tuple<Args &&...> args_tuple = std::forward_as_tuple(std::forward<Args>(args)...);
