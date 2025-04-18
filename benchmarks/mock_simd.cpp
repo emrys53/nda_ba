1459540bb499fd7202b91e41e08c873524a49d09
Fikret Ardal <fikret.ardal@tum.de>
2025-04-18 19:59:31 +0300

    mock_simd new benchmark

--- benchmarks/mock_simd.cpp
+++ benchmarks/mock_simd.cpp
@@ -16,38 +16,38 @@ const long Nmin = 16;
 const long Nmax = 1 << 20;
 
 template <typename Array, int Option>
-__attribute__((optimize("no-tree-vectorize")))
 static void mock_simd_benchmark(benchmark::State &state) {
   long N = state.range(0);
   auto A = Array::rand({N});
   auto B = Array::rand({N});
+  auto C = Array::rand({N});
   for (auto _ : state) {
 
     if constexpr (Option == 0) {
-        auto e1               = A + B;
-        auto e2               = nda::map(log_scalar{})(e1);
-        auto e3               = A * B + A;
-        auto e4               = e3 * A - B * e1;
-        auto e5               = e4 + e3;
-        auto e6               = e5 * e4;
-        auto e7               = e6 + e5;
-        auto e8               = e7 * e6;
-        auto e9               = e8 + e7;
-        auto e10              = e9 * e8 * e7 * e6 * e5 + e2;
-        volatile Array result = e10;
+      auto e1            = A + B;
+      auto e2            = e1 + A * B * (C + e1);
+      auto e3            = e2 * e1;
+      auto e4            = e3 * e2;
+      auto e5            = e4 + e3;
+      auto e6            = e5 + e4;
+      auto e7            = e6 + e5;
+      auto e8            = e7 + e6;
+      auto e9            = e8 + e7;
+      auto e10           = nda::map(log_scalar{})(e9);
+      volatile value_t f = nda::sum(e10);
 
     } else if constexpr (Option == 1) {
-      auto e1               = A + B;
-      auto e2               = nda::map(log_emulated{})(e1);
-      auto e3               = A * B + A;
-      auto e4               = e3 * A - B * e1;
-      auto e5               = e4 + e3;
-      auto e6               = e5 * e4;
-      auto e7               = e6 + e5;
-      auto e8               = e7 * e6;
-      auto e9               = e8 + e7;
-      auto e10              = e9 * e8 * e7 * e6 * e5 + e2;
-      volatile Array result = e10;
+      auto e1            = A + B;
+      auto e2            = e1 + A * B * (C + e1);
+      auto e3            = e2 * e1;
+      auto e4            = e3 * e2;
+      auto e5            = e4 + e3;
+      auto e6            = e5 + e4;
+      auto e7            = e6 + e5;
+      auto e8            = e7 + e6;
+      auto e9            = e8 + e7;
+      auto e10           = nda::map(log_emulated{})(e9);
+      volatile value_t f = nda::sum(e10);
     }
   }
 }
