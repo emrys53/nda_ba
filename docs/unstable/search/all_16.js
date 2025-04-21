c936714508a2a8fb93e2ac99c977bd24487ce369
emrys53 <93680530+emrys53@users.noreply.github.com>
2025-04-21 15:14:24 +0000

    Deploying to github.io from @ emrys53/nda_ba@51b1c1895d7a5bd85fbf663ea8756e608130a4a1 🚀

--- docs/unstable/search/all_16.js
+++ docs/unstable/search/all_16.js
@@ -73,14 +73,13 @@ var searchData=
   ['mpi_2ehpp_70',['mpi.hpp',['../mpi_8hpp.html',1,'']]],
   ['mpi_5fbroadcast_71',['mpi_broadcast',['../group__av__mpi.html#ga6df12000d826688a8203af93d6b6838e',1,'nda']]],
   ['mpi_5fgather_72',['mpi_gather',['../group__av__mpi.html#ga62aacd4df1ae34a7690e68d93c7429be',1,'nda']]],
-  ['mpi_5fgather_5fcapi_73',['mpi_gather_capi',['../group__av__mpi.html#gaf5fa72e07e5eb1972ebbc6e6396fe3b3',1,'nda']]],
+  ['mpi_5fgather_5finto_73',['mpi_gather_into',['../group__av__mpi.html#gabb73c0a539d4cd1edcad9c431ca716a4',1,'nda']]],
   ['mpi_5freduce_74',['mpi_reduce',['../group__av__mpi.html#ga3edd8f70f2213d38b391743ddab967b3',1,'nda']]],
-  ['mpi_5freduce_5fcapi_75',['mpi_reduce_capi',['../group__av__mpi.html#ga51224e436e40aad207aec1c7f237a7de',1,'nda']]],
-  ['mpi_5freduce_5fin_5fplace_76',['mpi_reduce_in_place',['../group__av__mpi.html#gac31ef46b7cffb19adbbb1ff06f9fcd8a',1,'nda']]],
-  ['mpi_5fscatter_77',['mpi_scatter',['../group__av__mpi.html#ga3c70e635ce6fd776980edc3b44f5823b',1,'nda']]],
-  ['mpi_5fscatter_5fcapi_78',['mpi_scatter_capi',['../group__av__mpi.html#ga9a693c512671b934773d5aad1a5a646f',1,'nda']]],
-  ['mpop_79',['mpop',['../group__utils__std.html#gaedd4bb9fe174afa880deb1d5ff1bfe32',1,'nda::stdutil']]],
-  ['multi_20dimensional_20indexing_80',['Multi-dimensional indexing',['../group__layout__idx.html',1,'']]],
-  ['multi_5fbucket_81',['multi_bucket',['../classnda_1_1mem_1_1multi__bucket.html',1,'nda::mem::multi_bucket&lt; ChunkSize &gt;'],['../classnda_1_1mem_1_1multi__bucket.html#a4acb0c47e202babde7a9588e6e46fd66',1,'nda::mem::multi_bucket::multi_bucket()'],['../classnda_1_1mem_1_1multi__bucket.html#af38beed4dcd7954d80c95c8c6d8d5e6f',1,'nda::mem::multi_bucket::multi_bucket(multi_bucket const &amp;)=delete'],['../classnda_1_1mem_1_1multi__bucket.html#a0497dbbb5c8868fe3d900ec14ac39f4c',1,'nda::mem::multi_bucket::multi_bucket(multi_bucket &amp;&amp;)=default']]],
-  ['multiplies_82',['multiplies',['../structnda_1_1clef_1_1tags_1_1multiplies.html',1,'nda::clef::tags']]]
+  ['mpi_5freduce_5finto_75',['mpi_reduce_into',['../group__av__mpi.html#ga663e553746cc0923df57b83c2c742eba',1,'nda']]],
+  ['mpi_5fscatter_76',['mpi_scatter',['../group__av__mpi.html#ga3c70e635ce6fd776980edc3b44f5823b',1,'nda']]],
+  ['mpi_5fscatter_5finto_77',['mpi_scatter_into',['../group__av__mpi.html#gae423dc9b0c850f1650fa2803b7123f80',1,'nda']]],
+  ['mpop_78',['mpop',['../group__utils__std.html#gaedd4bb9fe174afa880deb1d5ff1bfe32',1,'nda::stdutil']]],
+  ['multi_20dimensional_20indexing_79',['Multi-dimensional indexing',['../group__layout__idx.html',1,'']]],
+  ['multi_5fbucket_80',['multi_bucket',['../classnda_1_1mem_1_1multi__bucket.html',1,'nda::mem::multi_bucket&lt; ChunkSize &gt;'],['../classnda_1_1mem_1_1multi__bucket.html#a4acb0c47e202babde7a9588e6e46fd66',1,'nda::mem::multi_bucket::multi_bucket()'],['../classnda_1_1mem_1_1multi__bucket.html#af38beed4dcd7954d80c95c8c6d8d5e6f',1,'nda::mem::multi_bucket::multi_bucket(multi_bucket const &amp;)=delete'],['../classnda_1_1mem_1_1multi__bucket.html#a0497dbbb5c8868fe3d900ec14ac39f4c',1,'nda::mem::multi_bucket::multi_bucket(multi_bucket &amp;&amp;)=default']]],
+  ['multiplies_81',['multiplies',['../structnda_1_1clef_1_1tags_1_1multiplies.html',1,'nda::clef::tags']]]
 ];
